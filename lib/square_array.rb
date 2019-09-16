3cf8eb431d9cc2083a71026e9a03e6d621e3cb1c
Mustafa Muse <umpp101@icloud.com>
2019-09-16 08:46:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   newArray = []
   counter = 0
 while array[counter] do
-  newArray.push(array[counter] * array[counter])
+  newArray.push(array[counter] ** 2)
+  counter +=1
 end
 newArray
 end 
